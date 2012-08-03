2ad7525d7c1a803705f13740470b34a33a2f3758
Zach Hoeken <zach@makerbot.com>
2012-08-03 21:34:53 +0200

    more install stuff.

---
+++ install/first_boot.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+sudo apt-get update
+sudo apt-get install git apache2 php5-mysql
+
+git clone git://github.com/Hoektronics/BotQueue.git
+
+sudo cp ~/BotQueue/install/apache.conf /etc/apache2/sites-available/BotQueue
+sudo a2ensite BotQueue
+sudo /etc/init.d/apache2 restart
