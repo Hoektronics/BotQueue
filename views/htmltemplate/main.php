d7a43dc52b79df9998f8143a774e64f1419f8a64
Zach Hoeken <hoeken@gmail.com>
2012-08-09 19:29:14 +0000

    cleaned up header and footer stuff.

--- views/htmltemplate/main.php
+++ views/htmltemplate/main.php
@@ -2,7 +2,11 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title><?=$title?> - BotQueue.com</title>
+		<? if ($title): ?>
+			<title><?=strip_tags($title)?> // <?=RR_PROJECT_NAME?></title>
+		<? else: ?>
+			<title><?=RR_PROJECT_NAME?></title>
+		<? endif ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Zach Hoeken / BotQueue.com">
@@ -15,26 +19,39 @@
     <!-- Le styles -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="/css/docs.css" rel="stylesheet">
     <link href="/css/botqueue.css" rel="stylesheet">
 
-	 <script type="text/javascript">
+		<? if (defined('GOOGLE_ANALYTICS_TRACKING_CODE')): ?>
+		 <script type="text/javascript">
+		   var _gaq = _gaq || [];
+			  _gaq.push(['_setAccount', '<?=GOOGLE_ANALYTICS_TRACKING_CODE?>']);
+			  _gaq.push(['_setDomainName', "<?=SITE_HOSTNAME?>"]);
+	  		  _gaq.push(['_setAllowLinker', true]);
+			  _gaq.push(['_trackPageview']);
 
-		/*
-	   var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-23019901-1']);
-		  _gaq.push(['_setDomainName', "bootswatch.com"]);
-  		  _gaq.push(['_setAllowLinker', true]);
-		  _gaq.push(['_trackPageview']);
+		   (function() {
+		     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		   })();
+		 </script>
+		<? endif ?>
+		<? if (!empty(Controller::$rssFeeds)): ?>
+			<? foreach (Controller::$rssFeeds AS $feed): ?>
+				<link rel="alternate" type="application/rss+xml" title="<?= RR_PROJECT_NAME ?> - <?=$feed['title']?>" href="<?=$feed['url']?>" />
+			<? endforeach ?>
+		<? endif ?>
 
-	   (function() {
-	     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	   })();
-		*/
-	 </script>
-
+		<? if (IS_DEV_SITE): ?>
+			<style>
+				body
+				{
+					background-image: url("/img/devsite.png");
+					background-repeat: repeat-all;
+				}
+			</style>
+		<? endif ?>
+	  <?= Controller::$content_for["head"] ?>
   </head>
   <body class="preview" data-spy="scroll" data-target=".subnav" data-offset="50">
     <div class="container">
@@ -63,8 +80,7 @@
             </li>
             <li><a href="/queues">Queues</a></li>
             <li><a href="/bots">Bots</a></li>
-            <li><a href="/app">Apps</a></li>
-            <li><a href="/app">API</a></li>
+            <li><a href="/app">App</a></li>
             <li><a href="/help">Help</a></li>
           </ul>
           <ul class="nav pull-right">
@@ -73,7 +89,7 @@
 							<? if (User::isLoggedIn()): ?>
 	              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Hello, <?=User::$me->getName()?> <b class="caret"></b></a>
 	              <ul class="dropdown-menu">
-	                <li><a href="/settings">Settings</a></li>
+	                <li><a href="/preferences">Preferences</a></li>
 									<? if (User::isAdmin()): ?>
 	                	<li><a href="/admin">Admin</a></li>
 	                <? endif ?>
@@ -106,18 +122,17 @@
 	  <div class="span12">
 			<?=$content?>
 	  </div>
-	</div>
+	</div> <!-- end content -->
 </section>
 
 <br><br><br><br>
 
 <!-- Footer -->
-<!--
-<footer class="footer">
-	<a href="/api">API</a>
-	<p class="pull-right"><a href="#">Back to top</a></p>
-</footer>
--->
+<div class="footer">
+		&copy; <?= date("Y") ?> <?= COMPANY_NAME ?>. Generated in <?= round(microtime(true) - START_TIME, 2) ?> seconds.
+		
+		<?= Controller::$content_for['footer']; ?>
+</div>
 
 </div><!-- /container -->
 
