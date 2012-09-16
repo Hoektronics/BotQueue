97f57509c428ad24e0b11c5ffc59d9f1ae94f82e
Zach Hoeken <hoeken@gmail.com>
2012-09-16 07:53:18 +0000

    finished initial db schema

--- upgrade/v1tov2.sql
+++ upgrade/v1tov2.sql
@@ -1,43 +1,55 @@
 ALTER TABLE bots add `slice_config_id` int(11) unsigned NOT NULL;
+ALTER TABLE bots add key `slice_config_id` (`slice_config_id`);
 
 CREATE TABLE `slice_engines` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `engine_name` int(11) unsigned NOT NULL,
-  `engine_version` int(11) unsigned NOT NULL,
-  `engine_description` int(11) unsigned NOT NULL,
-  `is_featured` int(11) unsigned NOT NULL,
-  `is_public` int(11) unsigned NOT NULL,
+  `engine_name` varchar(255) NOT NULL,
+  `engine_path` varchar(255) NOT NULL,
+  `engine_version` varchar(255) NOT NULL,
+  `engine_description` text NOT NULL,
+  `is_featured` tinyint(1)  NOT NULL,
+  `is_public` tinyint(1) NOT NULL,
   `add_date` datetime NOT NULL,
-  `default_config_id` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  `default_config_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `engine_name` (`engine_name`),
+  KEY `is_featured` (`is_featured`),
+  KEY `is_public` (`is_public`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `slice_configs` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `fork_id` int(11) unsigned NOT NULL,
   `engine_id` int(11) unsigned NOT NULL,
   `user_id` int(11) unsigned NOT NULL,
-  `config_name` int(11) unsigned NOT NULL,
+  `config_name` varchar(255) NOT NULL,
+  `config_data` text NOT NULL,
   `add_date` datetime NOT NULL,
   `edit_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
+  KEY `fork_id` (`fork_id`),
   KEY `user_id` (`user_id`),
-  KEY `engine_id` (`engine_id`),
+  KEY `engine_id` (`engine_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `slice_jobs` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `job_id` int(11) unsigned NOT NULL,
   `input_id` int(11) unsigned NOT NULL,
   `output_id` int(11) unsigned NOT NULL,
+  `output_log` text NOT NULL,
   `slice_config_id` int(11) unsigned NOT NULL,
   `slice_config_snapshot` text NOT NULL,
   `worker_token` char(40) NOT NULL,
-  `output_log` text NOT NULL,
-  `status` enum('available', 'taken', 'complete', 'failure'),
+  `worker_name` varchar(255) NOT NULL,
+  `status` enum('available', 'slicing', 'complete', 'failure'),
   `progress` float NOT NULL DEFAULT '0',
   `add_date` datetime NOT NULL,
   `taken_date` datetime NOT NULL,
   `finish_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
-  KEY `engine_id` (`engine_id`),
+  KEY `job_id` (`job_id`),
+  KEY `slice_config_id` (`slice_config_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
