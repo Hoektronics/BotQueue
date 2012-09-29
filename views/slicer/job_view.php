a8782725a1a20a64dbead417dfde79bc2c7be8cc
Zach Hoeken <hoeken@gmail.com>
2012-09-29 02:23:23 +0000

    much better slicing flow now.

--- views/slicer/job_view.php
+++ views/slicer/job_view.php
@@ -5,6 +5,7 @@
   <h2>Oh no!  Something went wrong with the slicing process.</h3>
 
   <div class="alert alert-error">
+    <p>You should download the <a href="<?=$outputfile->getRealUrl()?>">output file</a> and verify that it is correct or not with a <a href="http://gcode.joewalnes.com/">gcode visualization tool</a>.</p>
     <p>Here is the error that the slice engine reported:</p>
     <blockquote><?= nl2br(Utility::sanitize($job->get('error_log')))?></blockquote>
   </div>
