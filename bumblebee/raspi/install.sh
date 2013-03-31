c3d54317dc91033d987109ea721e822c3d5e3f86
Zach Hoeken <zach@makerbot.com>
2013-03-31 14:04:06 +0800

    added 0.9.9 tag to slic3r script

--- bumblebee/raspi/install.sh
+++ bumblebee/raspi/install.sh
@@ -28,7 +28,7 @@ sudo cpanm Boost::Geometry::Utils Math::Clipper Math::ConvexHull Math::Geometry:
 cd /home/pi/
 git clone https://github.com/alexrj/Slic3r.git
 cd Slic3r
-#git checkout
+git checkout 0.9.9
 sudo perl Build.PL 
 sudo ./Build install
 
