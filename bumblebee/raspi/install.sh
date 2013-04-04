541240f00c07d42b222523ec11462eb2b62803ad
Zach Hoeken <zach@makerbot.com>
2013-04-04 18:28:59 +0800

    tweaked the install script.

--- bumblebee/raspi/install.sh
+++ bumblebee/raspi/install.sh
@@ -4,26 +4,33 @@
 sudo apt-get update -qy
 sudo apt-get upgrade -qy
 
-#install botqueue
-sudo apt-get install -qy git-core vim screen python-pip
-git clone git://github.com/Hoektronics/BotQueue.git
-sudo usermod -a -G dialout pi
-sudo pip install pyserial
-
-#install slic3r:
+#install slic3r dependencies:
 sudo apt-get install -qy git-core build-essential libgtk2.0-dev libwxgtk2.8-dev libwx-perl libmodule-build-perl libnet-dbus-perl cpanminus libextutils-cbuilder-perl gcc-4.7 g++-4.7 libwx-perl
-sudo cpanm Boost::Geometry::Utils Math::Clipper Math::ConvexHull Math::Geometry::Voronoi Math::PlanePath Moo Wx Math::ConvexHull::MonotoneChain 
+#sudo cpanm Wx Boost::Geometry::Utils Encode::Locale File::Basename File::Spec Getopt::Long Math::Clipper Math::ConvexHull Math::ConvexHull::MonotoneChain Math::Geometry::Voronoi Math::PlanePath Moo Scalar::Util Time::HiRes Test::More IO::Scalar
+sudo cpanm AAR/Boost-Geometry-Utils-0.06.tar.gz Math::Clipper Math::ConvexHull Math::ConvexHull::MonotoneChain Math::Geometry::Voronoi Math::PlanePath Moo
+
+# install Wx with a fake X framebuffer
+sudo apt-get install -qy xvfb
+sudo Xvfb :1 &
+sudo env DISPLAY=:1 cpanm Wx
+sudo killall Xvfb
+
+#install slic3r
 git clone https://github.com/alexrj/Slic3r.git
 cd Slic3r
 git checkout 0.9.9
 sudo perl Build.PL 
 sudo ./Build install
 
-#get it linked up and working on boot.
-sudo chmod a+x $HOME/BotQueue/bumblebee/raspi/bin/bumblebee
-sudo /bin/sh -c 'cat $HOME/BotQueue/bumblebee/raspi/inittab >> /etc/inittab'
+#get Botqueue linked up and working on boot.
+sudo apt-get install -qy git-core vim screen python-pip
+git clone git://github.com/Hoektronics/BotQueue.git
+sudo usermod -a -G dialout pi
+sudo pip install pyserial
+sudo /bin/sh -c 'cat /home/pi/BotQueue/bumblebee/raspi/inittab >> /etc/inittab'
+chmod a+x $HOME/BotQueue/bumblebee/raspi/bin/bumblebee
 cat $HOME/BotQueue/bumblebee/raspi/profile >> $HOME/.profile
-source $HOME/profile
+source $HOME/.profile
 
 #authorize our app now.
 clear
