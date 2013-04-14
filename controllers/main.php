9270db4d60d2724781663dccd964761220e82034
Zach Hoeken <hoeken@gmail.com>
2013-04-14 12:05:44 +0000

    added leaderboard to stats page.

--- controllers/main.php
+++ controllers/main.php
@@ -128,6 +128,14 @@
 		  $sql = "SELECT CEIL(SUM(unix_timestamp(end_date) - unix_timestamp(start_date)) / 3600) AS total FROM job_clock";
 		  $this->set('total_printing_time', db()->getValue($sql));
 		  
+		  //user leaderboard
+		  $sql = "SELECT CEIL(SUM(unix_timestamp(end_date) - unix_timestamp(start_date)) / 3600) AS total, user_id FROM job_clock WHERE status != 'working' GROUP BY user_id ORDER BY total DESC LIMIT 10";
+		  $this->set('user_leaderboard', db()->getArray($sql));
+
+		  //bot leaderboard
+		  $sql = "SELECT CEIL(SUM(unix_timestamp(end_date) - unix_timestamp(start_date)) / 3600) AS total, bot_id FROM job_clock WHERE status != 'working' GROUP BY bot_id ORDER BY total DESC LIMIT 10";
+		  $this->set('bot_leaderboard', db()->getArray($sql));
+
 		  if (User::isLoggedIn())
 		  {
   		  //active bots
--- views/main/stats.php
+++ views/main/stats.php
@@ -16,6 +16,7 @@
     total hours of printing time
   </div>
 </div>
+
 <? if (User::isLoggedIn()): ?>
   <br/><br/>
   <h1>My BotQueue.com Stats</h1>
@@ -38,3 +39,52 @@
     </div>
   </div>
 <? endif ?>
+
+<br/><br/>
+
+<div class="row">
+  <div class="span6">
+    <h1>User Leaderboard</h1>
+    <table class="table table-striped table-bordered table-condensed">
+    	<tbody>
+        <tr>
+          <th style="width: 50px">Rank</th>
+          <th>User</th>
+          <th>Hours</th>
+        </tr>
+        <? $rank=0 ?>
+        <? foreach ($user_leaderboard AS $row): ?>
+          <? $rank++ ?>
+          <? $user = new User($row['user_id']) ?>
+          <tr>
+            <td><?=$rank?></td>
+            <td><?=$user->getName()?></td>
+            <td><?=$row['total']?></td>
+        <? endforeach ?>
+      </tbody>
+    </table>
+  </div>
+  <div class="span6">
+    <h1>Bot Leaderboard</h1>
+    <table class="table table-striped table-bordered table-condensed">
+    	<tbody>
+        <tr>
+          <th style="width: 50px">Rank</th>
+          <th>Bot</th>
+          <th>Owner</th>
+          <th>Hours</th>
+        </tr>
+        <? $rank=0 ?>
+        <? foreach ($bot_leaderboard AS $row): ?>
+          <? $rank++ ?>
+          <? $bot = new Bot($row['bot_id']) ?>
+          <tr>
+            <td><?=$rank?></td>
+            <td><?=$bot->getName()?></td>
+            <td><?=$bot->getUser()->getName()?></td>
+            <td><?=$row['total']?></td>
+        <? endforeach ?>
+      </tbody>
+    </table>
+  </div>
+</div>
