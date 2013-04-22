a4e9ec11c7f4c96e2eac1650a2f411d20b8e0af7
Zach Hoeken <zach@makerbot.com>
2013-04-22 19:23:03 +0800

    fixed osx to resize and pad properly.

--- bumblebee/workerbee.py
+++ bumblebee/workerbee.py
@@ -214,7 +214,7 @@ class WorkerBee():
     self.checkMessages()
 
     #now pull in our new data.
-    self.data = resuself.changeStatus(result['data'])
+    self.data = self.changeStatus(result['data'])
     
     #notify the queen bee of our status.
     self.sendMessage('job_update', self.data['job'])
@@ -412,13 +412,12 @@ class WorkerBee():
         #what os are we using
         myos = hive.determineOS()
         if myos == "osx":
-          command = "exec ./imagesnap -q -d '%s' webcam.jpg && exec sips -Z 640 webcam.jpg" % (
+          command = "./imagesnap -q -d '%s' webcam.jpg && sips --resampleWidth 640 --padToHeightWidth 480 640 --padColor FFFFFF -s formatOptions 60%% webcam.jpg 2>/dev/null" % (
             self.config['webcam']['device']
           )
         elif myos == "raspberrypi" or os == "linux":
-          command = "exec /usr/bin/fswebcam -q --jpeg 60 -d %s -r %s --title '%s' webcam.jpg" % (
+          command = "exec /usr/bin/fswebcam -q --jpeg 60 -d %s -r 640x480 --title '%s' webcam.jpg" % (
             self.config['webcam']['device'],
-            self.config['webcam']['resolution'],
             "%s :: %0.2f%% :: BotQueue.com" % (self.config['name'], float(self.data['job']['progress']))
           )
         else:
