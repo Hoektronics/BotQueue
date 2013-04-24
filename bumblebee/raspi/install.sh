5c28f7b5f030f11b7cfe6a04ffd5cc95056cb71a
Zach Hoeken <zach@makerbot.com>
2013-04-24 15:27:54 +0800

    updated install instructions w/ new slic3r.

--- bumblebee/raspi/install.sh
+++ bumblebee/raspi/install.sh
@@ -3,35 +3,18 @@
 #make sure we're on the latest raspbian
 sudo apt-get update -qy
 sudo apt-get upgrade -qy
+
+#git is ridiculously important to us
 sudo apt-get install -qy git-core
 
-#install rpi-update
+#install rpi-update to update our firmware
 sudo wget http://goo.gl/1BOfJ -O /usr/bin/rpi-update
 sudo chmod +x /usr/bin/rpi-update
 sudo /usr/bin/rpi-update
 
-#install slic3r dependencies:
-sudo apt-get install -qy git-core build-essential libgtk2.0-dev libwxgtk2.8-dev libwx-perl libmodule-build-perl libnet-dbus-perl cpanminus libextutils-cbuilder-perl gcc-4.7 g++-4.7 libwx-perl
-#sudo cpanm Wx Boost::Geometry::Utils Encode::Locale File::Basename File::Spec Getopt::Long Math::Clipper Math::ConvexHull Math::ConvexHull::MonotoneChain Math::Geometry::Voronoi Math::PlanePath Moo Scalar::Util Time::HiRes Test::More IO::Scalar
-sudo cpanm AAR/Boost-Geometry-Utils-0.06.tar.gz Math::Clipper Math::ConvexHull Math::ConvexHull::MonotoneChain Math::Geometry::Voronoi Math::PlanePath Moo IO::Scalar Class::XSAccessor Growl::GNTP XML::SAX::ExpatXS
-
-# install Wx with a fake X framebuffer
-sudo apt-get install -qy xvfb
-sudo Xvfb :1 &
-sudo env DISPLAY=:1 cpanm Wx
-sudo killall Xvfb
-
-#install slic3r
-git clone https://github.com/alexrj/Slic3r.git
-cd Slic3r
-git checkout 0.9.9
-sudo perl Build.PL 
-sudo ./Build install
-
 #install webcam tools
-sudo apt-get install ipython python-opencv python-scipy python-numpy python-pygame python-setuptools python-pip
-sudo pip install https://github.com/ingenuitas/SimpleCV/zipball/master
-sudo apt-get install fswebcam uvcdynctrl
+#sudo apt-get install uvcdynctrl
+sudo apt-get install fswebcam 
 
 #get Botqueue linked up and working on boot.
 sudo apt-get install -qy git-core vim screen python-pip
