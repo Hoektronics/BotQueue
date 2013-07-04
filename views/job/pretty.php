e5f4c6e657a9517f41c82d0464d5037a7563d5bd
Zach Hoeken <hoeken@gmail.com>
2013-07-04 05:08:27 +0000

    added some extra pages that were around.

---
+++ views/job/pretty.php
@@ -0,0 +1,16 @@
+<div class="row">
+  <? foreach ($jobs AS $row): ?>
+    <? $job = $row['Job'] ?>
+    <? $webcam = $row['S3File'] ?>
+    <div class="span3 bot_thumbnail bot_thumbnail_<?=$size?>" style="margin-bottom: 30px; overflow: hidden;">
+      <div class="bot_thumbnail_content">
+        <div class="bot_thumbnail_stretcher"></div>
+        <div class="real_bot_thumbnail_content">
+          <a href="<?=$job->getUrl()?>">
+    	      <img src="<?=$webcam->getRealUrl()?>">
+      	  </a>
+      	</div>
+    	</div>
+  	</div>
+  <? endforeach ?>
+</div>
