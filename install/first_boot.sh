2682a8e780e9fc13448ac4e3868a24052af18589
Justin Nesselrotte <jnesselrotte@harding.edu@>
2014-01-05 08:45:05 -0600

    Changed first_boot file to allow further improvements

--- install/first_boot.sh
+++ install/first_boot.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+/bin/sh
 
 #    This file is part of BotQueue.
 #
@@ -16,11 +16,12 @@
 #    along with BotQueue.  If not, see <http://www.gnu.org/licenses/>.
   
 sudo apt-get update
+#TODO:Should these be passed in -y?
 sudo apt-get install git apache2 php5-mysql php-pear build-essential libpcre3-dev php5-curl
-
-sudo pecl install oauth
-
 sudo apt-get install libcurl3 php5-dev libcurl4-gnutls-dev libmagic-dev
+
+#TODO:the following 6 commands will fail if they're already installed
+sudo pecl install oauth
 sudo pecl install pecl_http
 
 sudo pear channel-discover pear.amazonwebservices.com
@@ -28,14 +29,28 @@ sudo pear channel-discover guzzlephp.org/pear
 sudo pear channel-discover pear.symfony.com
 sudo pear install aws/sdk
 
-git clone git://github.com/Hoektronics/BotQueue.git
-sudo cp ~/BotQueue/install/oauth.ini /etc/php5/apache2/conf.d/oauth.ini
+git status > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+  git clone git://github.com/Hoektronics/BotQueue.git
+  botqueue_dir="`pwd`/BotQueue"
+else
+  botqueue_dir="`pwd`/.." # We're in the install directory, so one level above
+fi
 
-sudo cp ~/BotQueue/install/apache.conf /etc/apache2/sites-available/BotQueue
+sudo cp "$botqueue_dir/install/oauth.ini" /etc/php5/apache2/conf.d/oauth.ini
+sudo cp "$botqueue_dir/install/apache.conf" /etc/apache2/sites-available/BotQueue
+
 sudo a2ensite BotQueue
 sudo a2enmod rewrite
 
 sudo /etc/init.d/apache2 reload
 sudo /etc/init.d/apache2 restart
 
-mysql -u root < install/createdb.sql
+#TODO:fix issue with database name not specified
+#TODO:fix issue with root user having a passward
+mysql -u root < "$botqueue_dir/install/createdb.sql"
+
+#TODO:Change apache.conf file in sites-available to reflect a possibly different host than http://botqueue.com
+#TODO:Change config.php to reflect host name, database name, user, and password changes here
+#TODO:Change client config in bumblebee to reflect this hostname
+
