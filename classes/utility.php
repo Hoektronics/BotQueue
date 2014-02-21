e02bfd8b34af4ab22f7658ee43f2969ae2c8ae4a
Justin Nesselrotte <jnesselr@harding.edu>
2014-02-21 13:06:41 -0600

    Created test for seconds, minutes, and days format

--- classes/utility.php
+++ classes/utility.php
@@ -69,24 +69,26 @@ class Utility
 		return (float)$converted_amount;
 	}
 
-	public function formatSeconds($seconds)
+	public static function formatSeconds($seconds)
 	{
 		if ($seconds == 0) {
 			$time = "Unknown";
 		} else {
-			$time = Utility::distance_of_time_in_words(time() - $seconds, time(), true);
+			$currentTime = time();
+			$time = Utility::distance_of_time_in_words($currentTime,
+				$currentTime + $seconds, true);
 		}
 
 		return $time;
 	}
 
 	// Returns the distance of time in words between two dates
-	public function distance_of_time_in_words($from_time, $to_time = null, $include_seconds = false)
+	public static function distance_of_time_in_words($from_time, $to_time = null, $include_seconds = false)
 	{
 		$to_time = $to_time ? $to_time : time();
 
-		$distance_in_minutes = floor(abs($to_time - $from_time) / 60);
 		$distance_in_seconds = floor(abs($to_time - $from_time));
+		$distance_in_minutes = floor($distance_in_seconds / 60);
 
 		$parameters = array();
 
@@ -94,13 +96,13 @@ class Utility
 			if (!$include_seconds) {
 				$string = $distance_in_minutes == 0 ? 'less than a minute' : '1 minute';
 			} else {
-				if ($distance_in_seconds <= 5) {
+				if ($distance_in_seconds < 5) {
 					$string = 'less than 5 seconds';
-				} else if ($distance_in_seconds >= 6 && $distance_in_seconds <= 10) {
+				} else if ($distance_in_seconds >= 5 && $distance_in_seconds <= 9) {
 					$string = 'less than 10 seconds';
-				} else if ($distance_in_seconds >= 11 && $distance_in_seconds <= 20) {
+				} else if ($distance_in_seconds >= 10 && $distance_in_seconds <= 19) {
 					$string = 'less than 20 seconds';
-				} else if ($distance_in_seconds >= 21 && $distance_in_seconds <= 40) {
+				} else if ($distance_in_seconds >= 20 && $distance_in_seconds <= 40) {
 					$string = 'half a minute';
 				} else if ($distance_in_seconds >= 41 && $distance_in_seconds <= 59) {
 					$string = 'less than a minute';
@@ -108,12 +110,12 @@ class Utility
 					$string = '1 minute';
 				}
 			}
-		} else if ($distance_in_minutes >= 2 && $distance_in_minutes <= 44) {
+		} else if ($distance_in_minutes >= 2 && $distance_in_minutes <= 59) {
 			$string = '%minutes% minutes';
 			$parameters['%minutes%'] = $distance_in_minutes;
-		} else if ($distance_in_minutes >= 45 && $distance_in_minutes <= 89) {
-			$string = 'about 1 hour';
-		} else if ($distance_in_minutes >= 90 && $distance_in_minutes <= 1439) {
+		} else if ($distance_in_minutes >= 60 && $distance_in_minutes <= 119) {
+			$string = "1 hour";
+		} else if ($distance_in_minutes >= 120 && $distance_in_minutes <= 1439) {
 			$string = 'about %hours% hours';
 			$parameters['%hours%'] = round($distance_in_minutes / 60);
 		} else if ($distance_in_minutes >= 1440 && $distance_in_minutes <= 2879) {
