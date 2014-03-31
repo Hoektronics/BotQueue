f8991b06ec6975944e5fc367a76cdc74950bb954
Justin Nesselrotte <jnesselr@harding.edu>
2014-03-31 00:49:59 -0500

    Removed bumblebee from this repo

--- bumblebee/drivers/makerbot_driver/Firmware/errors.py
+++
@@ -1,5 +0,0 @@
-class UnknownVersionError(KeyError):
-    """
-    An UnkonwnVersionError is thrown when a version is passed
-    into Uploader.py that is not found in a specific machine profile
-    """
