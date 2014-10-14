48e96bbebd2145ea4e315b8ec7effcf4509492fc
Justin Nesselrotte <jnesselr@harding.edu>
2014-10-14 15:26:38 -0500

    Fixing gcode parser

--- web/gcode-viewer/gcode-parser.js
+++ web/gcode-viewer/gcode-parser.js
@@ -6,36 +6,37 @@
  *   'default': Called if no other handler matches.
  */
 function GCodeParser(handlers) {
-  this.handlers = handlers || {};
+    this.handlers = handlers || {};
 }
 
-GCodeParser.prototype.parseLine = function(text, info) {
-  text = text.replace(/;.*$/, '').trim(); // Remove comments
-  if (text) {
-    var tokens = text.split(' ');
-    if (tokens) {
-      var cmd = tokens[0];
-      var args = {
-        'cmd': cmd
-      };
-      tokens.splice(1).forEach(function(token) {
-        var key = token[0].toLowerCase();
-        var value = parseFloat(token.substring(1));
-        args[key] = value;
-      });
-      var handler = this.handlers[tokens[0]] || this.handlers['default'];
-      if (handler) {
-        return handler(args, info);
-      }
+GCodeParser.prototype.parseLine = function (text, info) {
+    text = text.replace(/;.*$/, '').trim(); // Remove comments
+    if (text) {
+        var tokens = text.split(' ');
+        if (tokens) {
+            var cmd = tokens[0];
+            var args = {
+                'cmd': cmd
+            };
+            tokens.splice(1).forEach(function (token) {
+                if (!token == "") {
+                    var key = token[0].toLowerCase();
+                    args[key] = parseFloat(token.substring(1));
+                }
+            });
+            var handler = this.handlers[tokens[0]] || this.handlers['default'];
+            if (handler) {
+                return handler(args, info);
+            }
+        }
     }
-  }
 };
 
-GCodeParser.prototype.parse = function(gcode) {
-  var lines = gcode.split('\n');
-  for (var i = 0; i < lines.length; i++) {
-    if (this.parseLine(lines[i], i) === false) {
-      break;
+GCodeParser.prototype.parse = function (gcode) {
+    var lines = gcode.split('\n');
+    for (var i = 0; i < lines.length; i++) {
+        if (this.parseLine(lines[i], i) === false) {
+            break;
+        }
     }
-  }
 };
