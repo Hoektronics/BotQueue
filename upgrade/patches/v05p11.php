810bd971288b3d37c37c2947627225218d9ce47f
Justin Nesselrotte <jnesselr@harding.edu>
2015-02-13 15:03:05 -0600

    Adding index to the token in the db

---
+++ upgrade/patches/v05p11.php
@@ -0,0 +1,12 @@
+<?
+include("../patches.php");
+
+$patch = new Patch(23);
+
+if (!$patch->exists()) {
+
+	$sql = "ALTER table oauth_token ADD KEY `token` (`token`)";
+	db()->execute($sql);
+
+	$patch->finish("Adding MySQL keys");
+}
