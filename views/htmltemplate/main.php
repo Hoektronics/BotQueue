a4fb63a905d576920c76307219544eb2145921bc
Justin Nesselrotte <jnesselr@harding.edu>
2016-03-06 11:44:39 -0700

    Fixing favicon.ico location reference

--- views/htmltemplate/main.php
+++ views/htmltemplate/main.php
@@ -10,7 +10,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="Zach Hoeken / Justin Nesselrotte">
-	<link rel="icon" href="favicon.gif" type="image/gif">
+	<link rel="icon" href="/favicon.gif" type="image/gif">
 
 	<!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
 	<!--[if lt IE 9]>
