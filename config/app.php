edc588aee48030d0af17d9f0cb7c3d03e22aeee9
Justin Nesselrotte <jnesselr@harding.edu>
2017-05-24 02:08:49 -0600

    Adding IDE Helper

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Package Service Providers...
