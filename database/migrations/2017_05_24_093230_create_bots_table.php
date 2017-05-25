11987c0647cf1b1f6efa0c8f2a4f83849a42c8c5
Justin Nesselrotte <jnesselr@harding.edu>
2017-05-24 21:43:39 -0600

    Adding base bot page and migration

---
+++ database/migrations/2017_05_24_093230_create_bots_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bots', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->timestamp('seen_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bots');
+    }
+}
--- resources/views/bot/index.blade.php
+++ resources/views/bot/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-My bots yo
+    <div class="page-header">
+        <div class="btn-toolbar pull-right">
+            <a role="button" class="btn btn-primary btn-lg" href="/bot/create">Create a Bot</a>
+        </div>
+        <h1>Bots</h1>
+    </div>
 @endsection
