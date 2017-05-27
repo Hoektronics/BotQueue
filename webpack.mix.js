16e76854ebac6a9ffbaa50e9580dd253f8eca56c
Justin Nesselrotte <jnesselr@harding.edu>
2017-05-27 17:31:42 -0600

    Switching over to in app css files

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,4 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/app.scss', 'public/css');
