dbdbf524124f63361233f72690519e621854cb52
Justin Nesselrotte <jnesselr@harding.edu>
2017-06-21 00:49:30 -0600

    Fixing issue with extension validator on upload

--- app/Http/Requests/FileUploadRequest.php
+++ app/Http/Requests/FileUploadRequest.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Requests;
 
-use App\Validation\Validators\ExtensionValidator;
+use App\Validation\ExtensionValidator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 
