9453631d5a409a85b9d4c49bebd19e03d9d820ae
Justin Nesselrotte <jnesselr@harding.edu>
2018-02-17 15:29:14 -0700

    Adding host channel

--- routes/channels.php
+++ routes/channels.php
@@ -11,13 +11,20 @@
 |
 */
 
-Broadcast::channel('user.{user_id}', function ($user, $userId) {
+Broadcast::channel('user.{userId}', function ($user, $userId) {
     return (int) $user->id === (int) $userId;
 });
 
-Broadcast::channel('bot.{bot_id}', function ($user, $botId) {
+Broadcast::channel('bot.{botId}', function ($user, $botId) {
     /** @var \App\Bot $bot */
     $bot = \App\Bot::findOrNew($botId);
 
     return (int) $user->id === (int) $bot->creator_id;
+});
+
+Broadcast::channel('host.{hostId}', function ($user, $hostId) {
+    /** @var \App\Host $host */
+    $host = \App\Host::findOrNew($hostId);
+
+    return (int) $user->id === (int) $host->owner_id;
 });
