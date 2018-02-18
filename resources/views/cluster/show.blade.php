ca1d35992af4823d2659b3ce31df3b5097931e93
Justin Nesselrotte <jnesselr@harding.edu>
2018-02-17 21:41:00 -0700

    Updating cluster pages

--- resources/views/cluster/show.blade.php
+++ resources/views/cluster/show.blade.php
@@ -3,33 +3,32 @@
 @inject('bot_status', 'App\Services\BotStatusService')
 
 @section('content')
-    <div class="page-header">
-        <h1>{{ $cluster->name }}</h1>
-    </div>
-
     <div class="row">
         <div class="col-md-9">
+            <h1>{{ $cluster->name }}</h1>
+
             Main content
         </div>
+
         <div class="col-md-3">
-            <div class="panel panel-default">
-                <div class="panel-heading">
+            <div class="card mb-3">
+                <div class="card-header">
                     Info
                 </div>
-                <div class="panel-body">
+                <div class="card-body">
                     Creator: {{ $cluster->creator->username }}
                 </div>
             </div>
 
-            <div class="panel panel-default">
-                <div class="panel-heading">
+            <div class="card">
+                <div class="card-header">
                     Bots
                 </div>
-                <div class="panel-body">
+                <div class="card-body">
                     @foreach($cluster->bots as $bot)
                         <div class="row">
                             <h4>
-                                <a class="label {{ $bot_status->label_class($bot->status) }}"
+                                <a class="badge {{ $bot_status->label_class($bot->status) }}"
                                    href="{{ route('bots.show', [$bot]) }}">
                                     {{ $bot->name }}
                                 </a>
