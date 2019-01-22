5981ad78d86d01cde0073bb93128e9d615349a3e
Justin Nesselrotte <jnesselr@harding.edu>
2019-01-21 20:49:13 -0700

    Fixing escape characters in bot name

--- tests/Feature/Web/BotsTest.php
+++ tests/Feature/Web/BotsTest.php
@@ -108,7 +108,7 @@ class BotsTest extends TestCase
         $this
             ->actingAs($this->mainUser)
             ->get("/bots/{$bot->id}")
-            ->assertSee($bot->name)
+            ->assertSee(e($bot->name))
             ->assertSee($bot->status)
             ->assertSee("Creator: {$this->mainUser->name}");
     }
