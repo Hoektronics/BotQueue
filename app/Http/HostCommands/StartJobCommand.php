e7598c9d2d0bd82d45456a173fe0d5e5aa6df05b
Justin Nesselrotte <admin@jnesselr.org>
2019-03-04 22:04:14 -0700

    Added StartJobCommand

---
+++ app/Http/HostCommands/StartJobCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\HostCommands;
+
+
+use App\Enums\BotStatusEnum;
+use App\Enums\JobStatusEnum;
+use App\Errors\ErrorResponse;
+use App\Errors\HostErrors;
+use App\HostManager;
+use App\Http\Resources\JobResource;
+use App\Job;
+use Illuminate\Http\Response;
+use Illuminate\Support\Collection;
+
+class StartJobCommand
+{
+    use HostCommandTrait;
+
+    /**
+     * @var HostManager
+     */
+    private $hostManager;
+    /**
+     * @var HostErrors
+     */
+    private $hostErrors;
+
+    public function __construct(HostManager $hostManager,
+                                HostErrors $hostErrors)
+    {
+        $this->hostManager = $hostManager;
+        $this->hostErrors = $hostErrors;
+    }
+
+    /**
+     * @param $data Collection
+     * @return ErrorResponse|JobResource
+     */
+    public function __invoke($data)
+    {
+        $job = Job::find($data["id"]);
+
+        $currentStatus = $job->status;
+
+        abort_unless(JobStatusEnum::ASSIGNED == $currentStatus, Response::HTTP_CONFLICT);
+
+        $bot = $job->bot;
+
+        if($bot->host_id != $this->hostManager->getHost()->id) {
+            return $this->hostErrors->jobIsNotAssignedToThisHost();
+        }
+
+        $job->status = JobStatusEnum::IN_PROGRESS;
+        $bot->status = BotStatusEnum::WORKING;
+
+        $job->push();
+
+        return new JobResource($job);
+    }
+}
---
+++ tests/Feature/Host/Commands/StartJobCommandTest.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Tests\Feature\Host\Commands;
+
+use App\Enums\BotStatusEnum;
+use App\Enums\JobStatusEnum;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+use Tests\PassportHelper;
+
+class StartJobCommandTest extends TestCase
+{
+    use PassportHelper;
+
+    /** @test */
+    public function aHostCanUpdateJobStatusFromAssignedToInProgress()
+    {
+        $this->withoutJobs();
+
+        $bot = $this->bot()
+            ->state(BotStatusEnum::JOB_ASSIGNED)
+            ->host($this->mainHost)
+            ->create();
+
+        $file = $this->file()->gcode()->create();
+
+        $job = $this->job()
+            ->state(JobStatusEnum::ASSIGNED)
+            ->bot($bot)
+            ->file($file)
+            ->create();
+
+        $this
+            ->withTokenFromHost($this->mainHost)
+            ->postJson("/host", [
+                "command" => "StartJob",
+                "data" => [
+                    "id" => $job->id,
+                ],
+            ])
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJson([
+                "data" => [
+                    "id" => $job->id,
+                    "name" => $job->name,
+                    "status" => JobStatusEnum::IN_PROGRESS,
+                    "url" => $file->url(),
+                ]
+            ]);
+
+        $bot->refresh();
+        $job->refresh();
+
+        $this->assertEquals(JobStatusEnum::IN_PROGRESS, $job->status);
+        $this->assertEquals(BotStatusEnum::WORKING, $bot->status);
+    }
+
+    public static function nonAssignedJobStates()
+    {
+        return JobStatusEnum::allStates()
+            ->diff(JobStatusEnum::ASSIGNED)
+            ->reduce(function ($lookup, $item) {
+                $lookup[$item] = array($item);
+                return $lookup;
+            }, []);
+    }
+
+    /** @test
+     * @dataProvider nonAssignedJobStates
+     * @param $jobState
+     */
+    public function aHostCanNotGoFromANonAssignedStateToInProgress($jobState)
+    {
+        $this->withoutJobs();
+
+        $bot = $this->bot()
+            ->state(BotStatusEnum::JOB_ASSIGNED)
+            ->host($this->mainHost)
+            ->create();
+
+        $job = $this->job()
+            ->state($jobState)
+            ->bot($bot)
+            ->create();
+
+        $this
+            ->withExceptionHandling()
+            ->withTokenFromHost($this->mainHost)
+            ->postJson("/host", [
+                "command" => "StartJob",
+                "data" => [
+                    "id" => $job->id,
+                ],
+            ])
+            ->assertStatus(Response::HTTP_CONFLICT);
+    }
+
+    /** @test */
+    public function aHostCanNotUpdateAJobThatIsNotBeingWorkedOnByItsOwnBots()
+    {
+        $this->withoutJobs();
+
+        $host = $this->host()->create();
+
+        $bot = $this->bot()
+            ->state(BotStatusEnum::JOB_ASSIGNED)
+            ->host($host)
+            ->create();
+
+        $job = $this->job()
+            ->state(JobStatusEnum::ASSIGNED)
+            ->bot($bot)
+            ->create();
+
+        $this
+            ->withExceptionHandling()
+            ->withTokenFromHost($this->mainHost)
+            ->postJson("/host", [
+                "command" => "StartJob",
+                "data" => [
+                    "id" => $job->id,
+                ],
+            ])
+            ->assertStatus(Response::HTTP_FORBIDDEN);
+    }
+}
