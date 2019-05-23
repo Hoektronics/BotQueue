8cbfb88703aa3a293852f7f1a65a0bad3c3b814d
Justin Nesselrotte <admin@jnesselr.org>
2019-05-22 23:36:56 -0600

    Converted Host pages to Tailwind

--- resources/views/host/index.blade.php
+++ resources/views/host/index.blade.php
@@ -1,16 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Hosts</h1>
+    <div class="flex mx-4">
+        <span class="text-3xl">Hosts</span>
+    </div>
 
-    @if(count($hosts) == 0)
-        <h4>
-            No hosts available. See help to add one (TODO)
-        </h4>
-    @else
-        @foreach($hosts as $host)
-            {{--<a href="{{ route('hosts.show', [$host]) }}">{{ $host->name }}</a>--}}
-            <a>{{ $host->name }}</a><br>
-        @endforeach
-    @endif
+    <div class="flex flex-col mt-4">
+        @if(count($hosts) == 0)
+            <a class="text-xl mx-auto p-2 border border-red-300 rounded bg-red-300 text-center">
+                No hosts available. See help to add one (TODO)
+            </a>
+        @else
+            @foreach($hosts as $host)
+                <a {{-- href="{{ route('hosts.show', [$host]) }}" --}}
+                   class="text-xl m-2 p-2 border">
+                    {{ $host->name }}
+                </a>
+            @endforeach
+        @endif
+    </div>
 @endsection
--- resources/views/host/request/show.blade.php
+++ resources/views/host/request/show.blade.php
@@ -1,44 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row justify-content-md-center">
-        <div class="card w-50">
-            <div class="card-body">
-                <form class="form-horizontal" role="form" method="POST" action="{{ route('hosts.store') }}">
+    <div class="flex mx-4">
+        <div class="w-full rounded-lg border lg:w-1/3 lg:mx-auto">
+            <div class="text-center text-xl bg-gray-200">Claim Host</div>
+            <div class="p-4">
+                <form role="form" method="POST" action="{{ route('hosts.store') }}">
                     {{ csrf_field() }}
                     <input type="hidden" name="host_request_id" value="{{ $host_request->id }}">
 
-                    <div class="form-group">
-                        <label for="name" class="control-label">Host Name</label>
+                    <div class="flex mb-3">
+                        <label for="name" class="w-1/3">Host Name</label>
 
-                        <input name="name" type="text" value="{{ old('name', $host_request->hostname) }}"
-                               id="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" required autofocus>
+                        <div class="flex flex-col flex-grow">
+                            <input name="name" id="name" type="text"
+                                   value="{{ old('name', $host_request->hostname) }}"
+                                   class="my-auto border rounded"
+                                   required autofocus>
 
-                        @if ($errors->has('name'))
-                            <span class="form-text">
-                                <strong>{{ $errors->first('name') }}</strong>
-                            </span>
-                        @endif
+                            @if ($errors->has('name'))
+                                <span class="text-red-800">{{ $errors->first('name') }}</span>
+                            @endif
+                        </div>
                     </div>
 
                     @if($host_request->hostname !== null)
-                        <div>
-                            Device hostname: {{ $host_request->hostname }}
+                        <div class="flex mb-3">
+                            <label for="hostname" class="w-1/3">Device hostname</label>
+                            <input type="text" class="flex-grow" id="hostname" value="{{ $host_request->hostname }}" disabled>
                         </div>
                     @endif
 
                     @if($host_request->local_ip !== null)
-                        <div>
-                            Local IP: {{ $host_request->local_ip }}
+                        <div class="flex mb-3">
+                            <label for="local_ip" class="w-1/3">Local IP</label>
+                            <input type="text" class="flex-grow" id="local_ip" value="{{ $host_request->local_ip }}" disabled>
                         </div>
                     @endif
 
-                    @unless($host_request->local_ip === null && $host_request->hostname === null)
-                        <br>
-                    @endif
-
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary">
+                    <div class="flex justify-end mt-4">
+                        <button type="submit" class="btn-blue btn-lg btn-interactive">
                             Claim Host
                         </button>
                     </div>
