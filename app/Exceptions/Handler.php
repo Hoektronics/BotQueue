fcfc94bfce2aef20999a2786408336e9191487b4
Laravel Shift <shift@laravelshift.com>
2020-06-17 04:46:23 +0000

    Shift cleanup

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,10 +2,10 @@
 
 namespace App\Exceptions;
 
-use Throwable;
 use App\User;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
