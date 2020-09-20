826d4f08918df39f2352569cc6bb6b5c453eb7a6
Justin Nesselrotte <admin@jnesselr.org>
2020-09-20 12:50:07 -0600

    Removing side margins on upload field

--- resources/views/file/create.blade.php
+++ resources/views/file/create.blade.php
@@ -1,12 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex mx-4">
-        <div class="w-full rounded-lg border lg:w-1/3 lg:mx-auto">
-            <div class="text-center text-xl bg-gray-200">Upload a file</div>
-            <div class="p-4">
-                <file-upload token="{{ csrf_token() }}"></file-upload>
-            </div>
+    <div class="w-full rounded-lg border lg:w-1/3 lg:mx-auto">
+        <div class="text-center text-xl bg-gray-200">Upload a file</div>
+        <div class="p-4">
+            <file-upload token="{{ csrf_token() }}"></file-upload>
         </div>
     </div>
 @endsection
