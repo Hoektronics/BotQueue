a454d07ab61dcd746588cfdfdf88808cd2947149
Justin Nesselrotte <admin@jnesselr.org>
2020-09-29 20:47:52 -0600

    Removing css from email blade

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -1,8 +1,5 @@
 <!doctype html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <style> {!! readfile(public_path('css/app.css')) !!} </style>
-    </head>
     <body>
         @yield('content')
     </body>
